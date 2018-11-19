35491d521ad6b48f687a7906703d32afdb6dc4a0
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-11-19 09:38:43 +0200

    WIP: Added priorisation

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 import MapMarker from './MapMarker';
+import 'proj4';
+import 'proj4leaflet';
 
 import HarborMatchSelected from './common/icons/harbor-match-chosen.svg';
 import HarborMatchUnselected from './common/icons/harbor-unmatch.svg';
@@ -49,8 +51,36 @@ export default class MapCanvas extends Component<Props, State> {
   render() {
     const { filtered, selected, onClick } = this.props;
     const position = [this.state.lat, this.state.lng];
-
     const bounds = new L.LatLngBounds(filtered.map(berth => berth.location.coordinates).toArray());
+    const originNw = [bounds.getSouthWest(), bounds.getNorthEast()];
+
+    const crs = new L.Proj.CRS(
+      'EPSG:3067',
+      '+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+      {
+        transformation: new L.Transformation(1, -originNw[0], -1, originNw[1]),
+        bounds,
+        resolutions: [
+          8192,
+          4096,
+          2048,
+          1024,
+          512,
+          256,
+          128,
+          64,
+          32,
+          16,
+          8,
+          4,
+          2,
+          1,
+          0.5,
+          0.25,
+          0.125
+        ]
+      }
+    );
 
     const markerIcon = isSelected => {
       if (isSelected) {
@@ -61,13 +91,14 @@ export default class MapCanvas extends Component<Props, State> {
 
     return (
       <Map
+        crs={crs}
         bounds={bounds}
         boundsOptions={{ padding: [50, 50] }}
         center={position}
         zoom={this.state.zoom}
         style={style}
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer url="https://geoserver.hel.fi/mapproxy/wmts/osm-sm/etrs_tm35fin/{z}/{x}/{y}.png" />
         {filtered.map(berth => {
           const isSelected = selected && selected.includes(berth.identifier);
           return (
