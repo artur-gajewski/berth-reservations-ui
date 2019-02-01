68494b81e95a73840efd35d3ac205c0675d44a76
Ville Koivunen <ville.koivunen@hel.fi>
2019-02-01 17:25:38 +0200

    Read sentry environment-tag from environment variable

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import configureStore from './config/configureStore';
 
 import App from './components/containers/AppContainer';
 
-const { REACT_APP_PIWIK_URL, REACT_APP_PIWIK_ID, REACT_APP_SENTRY_DSN, NODE_ENV } = process.env;
+const { REACT_APP_PIWIK_URL, REACT_APP_PIWIK_ID, REACT_APP_SENTRY_DSN, REACT_APP_SENTRY_ENVIRONMENT } = process.env;
 
 const history = createHistory();
 
@@ -27,7 +27,7 @@ const piwik = PiwikReactRouter({
 });
 
 Sentry.init({
-  environment: NODE_ENV,
+  environment: REACT_APP_SENTRY_ENVIRONMENT,
   dsn: REACT_APP_SENTRY_DSN
 });
 
